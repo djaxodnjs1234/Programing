ac03a3ade64d47a77464da504099e36e6a356ad6
TaeWon <djaxodnjs1234@naver.com>
2022-03-10 22:55:17 +0900

    last commit

---
+++ ClassApp.java
@@ -0,0 +1,13 @@
+
+public class ClassApp {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.6)); //내림
+		System.out.println(Math.ceil(1.6)); //올림//클래스는 서로 연관된 변수와 매소드를 모아서 이름지은것
+
+		
+	}
+
+}
